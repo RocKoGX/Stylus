bbbde27593b17e928bc970f0ab7292c94253a581
Christian R. <joaquinromero24@hotmail.com>
2024-09-26 21:11:54 -0500

    Moodle Point Blank: Update
    - URLS de sonidos cambiados (algunos).
    - Se eliminó la parte de sonido para clics en botones (totalmente inservible, no se que hacia ahi).

--- Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
+++ Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
@@ -38,13 +38,13 @@
 
     // SONIDO PARA CLICKS EN ENLACES
     const sonidoClickEnlace = document.createElement("audio");
-    sonidoClickEnlace.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/b08437debf2b5ea1743e37942a22a743/PB_UI_Button_Click.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZzIMukLzMBt13OtnwQScDRg0TU6BIuedE2ApyIJUzHt7BszN1srV8BSyEvxEah5wH1VF0-xmXAkEmfnNY_6ApFbxYxJyM9iDwWLgCdDAa6NsaQceJ75E7HCSSD1vq05sWxRObRfKuTNKVlKR45WJgfY="; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
+    sonidoClickEnlace.src = "https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/PB_UI_Button_Click.wav"; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
     sonidoClickEnlace.preload = "auto";
     document.body.appendChild(sonidoClickEnlace);
 
     // SONIDO PARA CLICS EN ENLACES CANCEL
     const sonidoClickEnlaceCancel = document.createElement("audio");
-    sonidoClickEnlaceCancel.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/40ea5db79c29a7859fe7d932a69ed354/PB_UI_Button_Cancel.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ5SLI2kRrhQU4Eu3of8yCWq48uTadt4qZ-8jQAKQWY3YlovMPsUMzQzektefk9gpyyfqMpnZTp7f7tGVVTUSJaRRGLphyx1q2YBBVZb8UqZWL7oxYZUZ9PxM--kBAt9deUzeLYFEGn13uAm5v-zZzbk="; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
+    sonidoClickEnlaceCancel.src = "https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/PB_UI_Button_Cancel.wav"; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
     sonidoClickEnlaceCancel.preload = "auto";
     document.body.appendChild(sonidoClickEnlaceCancel);
 
@@ -54,15 +54,9 @@
     sonidoClickEnlaceOK.preload = "auto";
     document.body.appendChild(sonidoClickEnlaceOK);
 
-    // SONIDO PARA CLICKS EN BOTONES
-    const sonidoClickBoton = document.createElement("audio");
-    sonidoClickBoton.src = "https://files.voicy.network/public/Content/Clips/Sound/bd97e77f-3f95-4783-ad73-6ca25cd5afe6.mp3"; // Cambia esta URL por tu archivo de sonido para el clic en botones
-    sonidoClickBoton.preload = "auto";
-    document.body.appendChild(sonidoClickBoton);
-
     // SONIDO AL CARGAR LA PÁGINA
     const sonidoCargaPagina = document.createElement("audio");
-    sonidoCargaPagina.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/51ebcc5c9509189d8916512fedebb4a0/PB_UI_Page_Shift.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ7la9T6JqX850RTsx0w22wCP0dgKsjagxMnhPBVRNA6uCN8ZDSc7t7XV6cLaDFALwJPY_dYCdT8bsiJQs_GN-0o55zrx5rHswFa1X-_O0ijH5F05RlW6MgU9lZo8lOH7LWRG2uQoE_uCXWRNkU79Tu0="; // Cambia esta URL por tu archivo de sonido
+    sonidoCargaPagina.src = "https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/PB_UI_Page_Shift.mp3"; // Cambia esta URL por tu archivo de sonido
     sonidoCargaPagina.preload = "auto";
     document.body.appendChild(sonidoCargaPagina);
 
@@ -103,7 +97,7 @@
         }
         else{
         enlace.addEventListener("click", function(e) {
-            const sonidoClick = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/b08437debf2b5ea1743e37942a22a743/PB_UI_Button_Click.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZzIMukLzMBt13OtnwQScDRg0TU6BIuedE2ApyIJUzHt7BszN1srV8BSyEvxEah5wH1VF0-xmXAkEmfnNY_6ApFbxYxJyM9iDwWLgCdDAa6NsaQceJ75E7HCSSD1vq05sWxRObRfKuTNKVlKR45WJgfY=");
+            const sonidoClick = new Audio("https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/PB_UI_Button_Click.wav");
             sonidoClick.play().catch((error) => {
                 console.error('Error al intentar reproducir el sonido de clic en enlace:', error);
             });
