f2640a9f0d4d3d49ac075c2b1efb0397cf8021e5
Christian R. <joaquinromero24@hotmail.com>
2024-09-28 00:16:56 -0500

    Moodle Point Blank: Update CSS y JS
    - CSS: Se añadió color degradado a la alerta amarilla.
    - JS: Sonido para animación de salida, nuevos sonidos agregados.

--- Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
+++ Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Point Blank UI Sounds for Moodle
 // @namespace    http://tampermonkey.net/
-// @version      2.1
+// @version      3.0
 // @description  Sonidos de la UI del juego ya extinto Point Blank
 // @author       RocKo
 // @match        https://aulavirtual.ucsm.edu.pe/*
@@ -12,27 +12,33 @@
     'use strict';
 
 
-  document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-link, .moremenu .nav-link').forEach(link => {
-    link.addEventListener('click', function(e) {
-      const targetUrl = this.href;
+document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-link, .moremenu .nav-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const targetUrl = this.href;
 
-      // Verificamos que el enlace tenga un href válido y no sea igual a la URL actual
-      if (targetUrl && targetUrl !== '#' && targetUrl !== window.location.href) {
-        e.preventDefault(); // Previene la redirección inmediata
+    // Verificamos que el enlace tenga un href válido y no sea igual a la URL actual
+    if (targetUrl && targetUrl !== '#' && targetUrl !== window.location.href) {
+      e.preventDefault(); // Previene la redirección inmediata
 
-        // Retrasamos 1 segundo antes de aplicar la animación
+      // Retrasamos 1 segundo antes de aplicar la animación
+      setTimeout(() => {
+        // Añadimos la clase que dispara la animación de deslizamiento
+        document.querySelector('.main-inner').classList.add('slide-out');
+
+        // Crear el elemento de audio para el sonido de la animación
+        const sonidoAnimacion = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/53199659f49dd3af6c61884c2aa9785a/UI_Scroll_Out.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZyiDfiPOmANuacXhlJZZauXH_doIhmJeDo1h3n7ty0NaReKu5wvBgRLvEf9XBaRPjvfDyY1574hG1_1aXpw523T8t9udUF0dTtcrug2tJAN7rUaZpTo9kXtAQdXXPX6NZA=="); // Cambia esta URL por tu archivo de sonido
+        sonidoAnimacion.play().catch((error) => {
+          console.error('Error al intentar reproducir el sonido de animación:', error);
+        });
+
+        // Esperamos a que la animación termine antes de redirigir
         setTimeout(() => {
-          // Añadimos la clase que dispara la animación de deslizamiento
-          document.querySelector('.main-inner').classList.add('slide-out');
-
-          // Esperamos a que la animación termine antes de redirigir
-          setTimeout(() => {
-            window.location.href = targetUrl; // Redirigimos a la nueva página
-          }, 1); // El tiempo de la animación (1s)
-        }, 600); // Esperamos 1 segundo antes de iniciar la animación
-      }
-    });
+          window.location.href = targetUrl; // Redirigimos a la nueva página
+        }, 1000); // El tiempo de la animación (1s)
+      }, 600); // Esperamos 600ms antes de iniciar la animación
+    }
   });
+});
 
 
 
@@ -54,9 +60,15 @@
     sonidoClickEnlaceOK.preload = "auto";
     document.body.appendChild(sonidoClickEnlaceOK);
 
+    // SONIDO PARA CLICS EN ENLACES TAB
+    const sonidoClickEnlaceTab = document.createElement("audio");
+    sonidoClickEnlaceTab.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/f470ee7553c66bb6b7536911cb9ca4f0/UI_Button_Click_Tab.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ8Gg775Lh1Q3xARJYXY1aTBXhctf3DOERkQYBIRDqvOTgpHLys27KI2-9BJp25Fl942gUa1zyJRgb_nJsjwNFWofRk8c8ubDrGnTunM6f_BYOwthDQ9MFYg9hdDVL26bVQ5hAlGOlX2ougUGBLOUh8Q="; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
+    sonidoClickEnlaceTab.preload = "auto";
+    document.body.appendChild(sonidoClickEnlaceTab);
+
     // SONIDO AL CARGAR LA PÁGINA
     const sonidoCargaPagina = document.createElement("audio");
-    sonidoCargaPagina.src = "https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/PB_UI_Page_Shift.mp3"; // Cambia esta URL por tu archivo de sonido
+    sonidoCargaPagina.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/7593384e5e2d90770218da6a56bcdbca/UI_Scroll_In.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZxZ6skmFwxR7pFQFU6PthJokbnFsCkxfQ32fEqJr5Pv04a8Wm1NhpXY-U9jitv6daLm3rjiEHkCiARwaBFxRdCC16X0Uqz8J9mZlY1ds6i1EVem2JMTCQpID8F8bPfp1kA=="; // Cambia esta URL por tu archivo de sonido
     sonidoCargaPagina.preload = "auto";
     document.body.appendChild(sonidoCargaPagina);
 
@@ -91,6 +103,13 @@
         else if(enlace.id.startsWith("id_submitbutton") || enlace.classList.contains("mod_quiz-next-nav")){
         enlace.addEventListener("click", function(e) {
             sonidoClickEnlaceOK.play().catch((error) => {
+                console.error('Error al intentar reproducir el sonido de clic en enlace:', error);
+            });
+        });
+        }
+        else if (enlace.closest('.moremenu') && enlace.classList.contains("nav-link")) {
+        enlace.addEventListener("click", function(e) {
+            sonidoClickEnlaceTab.play().catch((error) => {
                 console.error('Error al intentar reproducir el sonido de clic en enlace:', error);
             });
         });
