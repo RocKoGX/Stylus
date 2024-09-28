f7e7ab741d885af4fd2b41f056638ebb6ccdc53d
Christian R. <joaquinromero24@hotmail.com>
2024-09-28 10:18:24 -0500

    Moodle Point Blank JS Update 3.1
    - Se mejoró el sonido al cargar la pagina.
    - Al hacer clic en un dropdown toggle en celular este hacia la animación que no debería. fue corregido.
    - Sonido de hover y clic para nueva clase nav-link

--- Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
+++ Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Point Blank UI Sounds for Moodle
 // @namespace    http://tampermonkey.net/
-// @version      3.0
+// @version      3.1
 // @description  Sonidos de la UI del juego ya extinto Point Blank
 // @author       RocKo
 // @match        https://aulavirtual.ucsm.edu.pe/*
@@ -11,36 +11,38 @@
 (function() {
     'use strict';
 
+    document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-link, .moremenu .nav-link, .dropdown-toggle').forEach(link => {
+        link.addEventListener('click', function(e) {
+            // Si el enlace tiene la clase 'dropdown-toggle', no realizamos la animación
+            if (this.classList.contains('dropdown-toggle')) {
+                return; // Salimos del evento sin hacer nada
+            }
 
-document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-link, .moremenu .nav-link').forEach(link => {
-  link.addEventListener('click', function(e) {
-    const targetUrl = this.href;
+            const targetUrl = this.href;
 
-    // Verificamos que el enlace tenga un href válido y no sea igual a la URL actual
-    if (targetUrl && targetUrl !== '#' && targetUrl !== window.location.href) {
-      e.preventDefault(); // Previene la redirección inmediata
+            // Verificamos que el enlace tenga un href válido y no sea igual a la URL actual
+            if (targetUrl && targetUrl !== '#' && targetUrl !== window.location.href) {
+                e.preventDefault(); // Previene la redirección inmediata
 
-      // Retrasamos 1 segundo antes de aplicar la animación
-      setTimeout(() => {
-        // Añadimos la clase que dispara la animación de deslizamiento
-        document.querySelector('.main-inner').classList.add('slide-out');
+                // Retrasamos 1 segundo antes de aplicar la animación
+                setTimeout(() => {
+                    // Añadimos la clase que dispara la animación de deslizamiento
+                    document.querySelector('.main-inner').classList.add('slide-out');
 
-        // Crear el elemento de audio para el sonido de la animación
-        const sonidoAnimacion = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/53199659f49dd3af6c61884c2aa9785a/UI_Scroll_Out.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZyiDfiPOmANuacXhlJZZauXH_doIhmJeDo1h3n7ty0NaReKu5wvBgRLvEf9XBaRPjvfDyY1574hG1_1aXpw523T8t9udUF0dTtcrug2tJAN7rUaZpTo9kXtAQdXXPX6NZA=="); // Cambia esta URL por tu archivo de sonido
-        sonidoAnimacion.play().catch((error) => {
-          console.error('Error al intentar reproducir el sonido de animación:', error);
+                    // Crear el elemento de audio para el sonido de la animación
+                    const sonidoAnimacion = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/53199659f49dd3af6c61884c2aa9785a/UI_Scroll_Out.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZyiDfiPOmANuacXhlJZZauXH_doIhmJeDo1h3n7ty0NaReKu5wvBgRLvEf9XBaRPjvfDyY1574hG1_1aXpw523T8t9udUF0dTtcrug2tJAN7rUaZpTo9kXtAQdXXPX6NZA=="); // Cambia esta URL por tu archivo de sonido
+                    sonidoAnimacion.play().catch((error) => {
+                        console.error('Error al intentar reproducir el sonido de animación:', error);
+                    });
+
+                    // Esperamos a que la animación termine antes de redirigir
+                    setTimeout(() => {
+                        window.location.href = targetUrl; // Redirigimos a la nueva página
+                    }, 1000); // El tiempo de la animación (1s)
+                }, 600); // Esperamos 600ms antes de iniciar la animación
+            }
         });
-
-        // Esperamos a que la animación termine antes de redirigir
-        setTimeout(() => {
-          window.location.href = targetUrl; // Redirigimos a la nueva página
-        }, 1000); // El tiempo de la animación (1s)
-      }, 600); // Esperamos 600ms antes de iniciar la animación
-    }
-  });
-});
-
-
+    });
 
     // SONIDO PARA CLICKS EN ENLACES
     const sonidoClickEnlace = document.createElement("audio");
@@ -66,18 +68,9 @@ document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-lin
     sonidoClickEnlaceTab.preload = "auto";
     document.body.appendChild(sonidoClickEnlaceTab);
 
-    // SONIDO AL CARGAR LA PÁGINA
-    const sonidoCargaPagina = document.createElement("audio");
-    sonidoCargaPagina.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/7593384e5e2d90770218da6a56bcdbca/UI_Scroll_In.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZxZ6skmFwxR7pFQFU6PthJokbnFsCkxfQ32fEqJr5Pv04a8Wm1NhpXY-U9jitv6daLm3rjiEHkCiARwaBFxRdCC16X0Uqz8J9mZlY1ds6i1EVem2JMTCQpID8F8bPfp1kA=="; // Cambia esta URL por tu archivo de sonido
-    sonidoCargaPagina.preload = "auto";
-    document.body.appendChild(sonidoCargaPagina);
-
-    // Reproducir el sonido al cargar la página
-    window.onload = function() {
-        sonidoCargaPagina.play().catch((error) => {
-            console.error('Error al intentar reproducir el sonido de carga de la página:', error);
-        });
-    };
+    // SONIDO PARA CAMBIO DE PAGINA
+    const sonidoAnimacionEntrada = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/7593384e5e2d90770218da6a56bcdbca/UI_Scroll_In.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZxZ6skmFwxR7pFQFU6PthJokbnFsCkxfQ32fEqJr5Pv04a8Wm1NhpXY-U9jitv6daLm3rjiEHkCiARwaBFxRdCC16X0Uqz8J9mZlY1ds6i1EVem2JMTCQpID8F8bPfp1kA==");
+    sonidoAnimacionEntrada.play().catch((error) => console.error('Error:', error));
 
     // Seleccionar todos los enlaces de los cursos dentro de varias listas
     //
@@ -90,7 +83,7 @@ document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-lin
     // .popover-region                                Boton Chat
     // .activity.activity-wrapper                     Enlaces para cada contenido de un curso
 
-    const enlaces = document.querySelectorAll(".btnlogeo, #myCoursesList .rui-sidebar-nav-item-link, .rui-flatnavigation .rui-sidebar-nav-item-link, #myCoursesBtn, .moremenu .nav-link, .rui-lang-btn, .rui-icon-menu-darkmode, #nav-notification-popover-container, .popover-region, .btn, #id_cancel, id_submitbutton, .btn-danger, .btn-msg-danger, .userpicture, #mainNav, .list-group, .btn-msg, .btn-msg-special, .content-item-container, .dropdown-item, .mod_quiz-next-nav, .questionflag, .btn .btn-secondary .btn-sm, #mobileNav");
+    const enlaces = document.querySelectorAll(".btnlogeo, #myCoursesList .rui-sidebar-nav-item-link, .rui-flatnavigation .rui-sidebar-nav-item-link, #myCoursesBtn, .moremenu .nav-link, .rui-lang-btn, .rui-icon-menu-darkmode, #nav-notification-popover-container, .popover-region, .btn, #id_cancel, id_submitbutton, .btn-danger, .btn-msg-danger, .userpicture, #mainNav, .list-group, .btn-msg, .btn-msg-special, .content-item-container, .dropdown-item, .mod_quiz-next-nav, .questionflag, .btn .btn-secondary .btn-sm, #mobileNav, .main-content .nav-link");
     // Añadir el evento click a cada enlace para reproducir el sonido de clic
     enlaces.forEach(enlace => {
         if(enlace.id.startsWith("id_cancel") || enlace.classList.contains("btn-danger") || enlace.classList.contains("btn-msg-danger")){
@@ -107,7 +100,7 @@ document.querySelectorAll('.rui-sidebar-nav-item-link, .rui-sidebar-nav-item-lin
             });
         });
         }
-        else if (enlace.closest('.moremenu') && enlace.classList.contains("nav-link")) {
+        else if (enlace.closest('.moremenu') && enlace.classList.contains("nav-link") || enlace.closest('.main-content') && enlace.classList.contains("nav-link")) {
         enlace.addEventListener("click", function(e) {
             sonidoClickEnlaceTab.play().catch((error) => {
                 console.error('Error al intentar reproducir el sonido de clic en enlace:', error);
