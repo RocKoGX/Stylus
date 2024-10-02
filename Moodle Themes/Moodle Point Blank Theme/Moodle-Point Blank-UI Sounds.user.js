39275d8ed48454891c94f48f8cbad055d1775a21
Christian R. <joaquinromero24@hotmail.com>
2024-10-01 23:19:45 -0500

    Moodle Point Blank JS 3.2 Update
    - URL de algunos sonidos cambiados por el del repositorio.

--- Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
+++ Moodle Themes/Moodle Point Blank Theme/Moodle-Point Blank-UI Sounds.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Point Blank UI Sounds for Moodle
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  Sonidos de la UI del juego ya extinto Point Blank
 // @author       RocKo
 // @match        https://aulavirtual.ucsm.edu.pe/*
@@ -30,7 +30,7 @@
                     document.querySelector('.main-inner').classList.add('slide-out');
 
                     // Crear el elemento de audio para el sonido de la animación
-                    const sonidoAnimacion = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/53199659f49dd3af6c61884c2aa9785a/UI_Scroll_Out.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZyiDfiPOmANuacXhlJZZauXH_doIhmJeDo1h3n7ty0NaReKu5wvBgRLvEf9XBaRPjvfDyY1574hG1_1aXpw523T8t9udUF0dTtcrug2tJAN7rUaZpTo9kXtAQdXXPX6NZA=="); // Cambia esta URL por tu archivo de sonido
+                    const sonidoAnimacion = new Audio("https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/UI_Scroll_Out.wav"); // Cambia esta URL por tu archivo de sonido
                     sonidoAnimacion.play().catch((error) => {
                         console.error('Error al intentar reproducir el sonido de animación:', error);
                     });
@@ -58,18 +58,18 @@
 
     // SONIDO PARA CLICS EN ENLACES OK
     const sonidoClickEnlaceOK = document.createElement("audio");
-    sonidoClickEnlaceOK.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/301faa0ea33d8a61859fb8b4b1b85452/PB_UI_Button_Click_Ok.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ4Twibr-X-Bv3p3LLkQ4zRt6zyVPApLL4RjjM5N_uWXO3VR7HNsLNTs7IrzFl9m2wn-cJai2KV1EUwT2JQcZgq_cNsft4lFSU0KcXsnT0gJ0koGeVdn6EdQwFP0AMBe0dNiq0R9NLUaRaNsSyWkKx4o="; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
+    sonidoClickEnlaceOK.src = "https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/PB_UI_Button_Click_Ok.wav"; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
     sonidoClickEnlaceOK.preload = "auto";
     document.body.appendChild(sonidoClickEnlaceOK);
 
     // SONIDO PARA CLICS EN ENLACES TAB
     const sonidoClickEnlaceTab = document.createElement("audio");
-    sonidoClickEnlaceTab.src = "https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/f470ee7553c66bb6b7536911cb9ca4f0/UI_Button_Click_Tab.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ8Gg775Lh1Q3xARJYXY1aTBXhctf3DOERkQYBIRDqvOTgpHLys27KI2-9BJp25Fl942gUa1zyJRgb_nJsjwNFWofRk8c8ubDrGnTunM6f_BYOwthDQ9MFYg9hdDVL26bVQ5hAlGOlX2ougUGBLOUh8Q="; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
+    sonidoClickEnlaceTab.src = "https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/UI_Button_Click_Tab.wav"; // Cambia esta URL por tu archivo de sonido para el clic en enlaces
     sonidoClickEnlaceTab.preload = "auto";
     document.body.appendChild(sonidoClickEnlaceTab);
 
     // SONIDO PARA CAMBIO DE PAGINA
-    const sonidoAnimacionEntrada = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/7593384e5e2d90770218da6a56bcdbca/UI_Scroll_In.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZxZ6skmFwxR7pFQFU6PthJokbnFsCkxfQ32fEqJr5Pv04a8Wm1NhpXY-U9jitv6daLm3rjiEHkCiARwaBFxRdCC16X0Uqz8J9mZlY1ds6i1EVem2JMTCQpID8F8bPfp1kA==");
+    const sonidoAnimacionEntrada = new Audio("https://github.com/RocKoGX/Stylus/raw/main/Moodle%20Themes/Moodle%20Point%20Blank%20Theme/sounds/UI_Scroll_In.wav");
     sonidoAnimacionEntrada.play().catch((error) => console.error('Error:', error));
 
     // Seleccionar todos los enlaces de los cursos dentro de varias listas
@@ -117,7 +117,7 @@
         }
         // SONIDO PARA CLICKS EN HOVER
         enlace.addEventListener("mouseenter", function(e) {
-            const sonidoHover = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/7785f908eae97527ab63d2a464988b03/PB_UI_Button_Hover_2.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ7KIMSzRKn2FMv5xJqCzcx16O44hyfkgLqLQu0a9Ktl-H-vaI3Ef3fXQ_Rw33Zp1az9CnIdrukQEn-hYdO4PDvnFLE-Tf3AM6kDcZ4DyJbLmLLvTsxQ6IzCEPyU-q4GBXnICoNnOK4qpcNnvvKWVi3Q="); // Cambia esta URL por tu archivo de sonido para el hover
+            const sonidoHover = new Audio("https://u1.padletusercontent.com/uploads/padlet-uploads/500569185/7785f908eae97527ab63d2a464988b03/PB_UI_Button_Hover_2.mp3?expiry_token=5WaHZRdGG3LkUVQGy3SZ-zdRtq89aJeottSBaF_Hii8EGDVBG-vnLc5ZfL_2GiKosWMOCkHArMcc8LorETHcZ7KIMSzRKn2FMv5xJqCzcx16O44hyfkgLqLQu0a9Ktl-H-vaI3Ef3fXQ_Rw33Zp1az9CnIdrukQEn-hYdO4PDvnFLE-Tf3AM6kDcZ4DyJbLmc25DcoDPtKd3opKg6jhQJJQg-mIMgoLDdgSspAcvygE="); // Cambia esta URL por tu archivo de sonido para el hover
             sonidoHover.play().catch((error) => {
                 console.error('Error al intentar reproducir el sonido de hover:', error);
             });
